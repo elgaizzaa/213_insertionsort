48540479eef80f992fcd47b5350370baaa04a516
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-03-14 14:52:00 +0700

    step 2-4b

--- 213_insertionsore/213_insertionsore.cpp
+++ 213_insertionsore/213_insertionsore.cpp
@@ -9,7 +9,15 @@ void insertionsort() {
     int arr[40];
 
     for(i =1; i<= n-1; i++) { //step1
-     
+        temp = arr[i]; //step 2
+
+        j = i - 1; //step 3
+
+        while (j >= 0 && arr[j] > temp) //step4
+        {
+            arr[j + 1] = arr[j]; //step 4a
+            j--; //step 4b 
+        }
     }
 }
 int main()
