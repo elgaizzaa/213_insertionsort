8180aa42f3094baa0899ecdf66839bee1eb7cfa9
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-03-14 15:13:43 +0700

    menambahkan display

--- 213_insertionsore/213_insertionsore.cpp
+++ 213_insertionsore/213_insertionsore.cpp
@@ -24,6 +24,7 @@ void input() {
     for (int i = 0; i < n; i++) {
         cout << "data ke-" << (i + 1) << ": ";
         cin >> arr[i];
+
 void insertionsort() {
     int i, n, j, temp;
     int arr[40];
@@ -41,8 +42,22 @@ void insertionsort() {
 
             arr[j + 1] = temp; //step 5
          }
+    void display() {
+        cout << endl;
+        cout << "===================================" << endl;
+        cout << "Element Array yang telah tersusun" << endl;
+        cout << "===================================" << endl;
+        for (int j = 0; j < n; j++) {
+            cout << arr[j] << endl; // Output setiap element array pada garis baru
+
+        }
+        cout << "jumlah pass = " << n - 1 << endl; //menampilkan total angka dari elemen yang benar
+        cout << endl;
+    }
+   
 }
 int main()
+input();
 
 
 {
